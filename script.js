6f9cad32b92b06d68213b1b6e479039a4b8259ae
Abiodun David Ajirotutu <77601583+Davidajirotutu@users.noreply.github.com>
2021-01-22 15:52:33 -0600

    Update navbar width to 250px

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ var menuBtn = document.getElementById("menuBtn");
 var menuBtn = document.getElementById("sideNav");
 var menuBtn = document.getElementById("menu");
 
-sideNav.style.right = "-150px";
+sideNav.style.right = "-250px";
 
 menuBtn.onclick = function () {
-    if(sideNav.style.right === "-150px"){
+    if(sideNav.style.right === "-250px"){
         sideNav.style.right = "0";
         menu.src = "close.png";
     }
     else{
-        sideNav.style.right = "-150px";
+        sideNav.style.right = "-250px";
         menu.src = "menu.png";
     }
 }
